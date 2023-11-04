5db6e709a357c95dc86b6cb8bc24d59a3294fec1
KelterenB <no@name.no>
2023-11-04 10:17:24 +0100

    completed solution

--- problem10.py
+++ problem10.py
@@ -4,4 +4,28 @@ Find the sum of all the primes below two million.
 
 """
 
-# solution goes here
+limit = 2000000
+sumPrimes = 0
+
+def isPrime(num):
+    if num <= 1:
+        return False
+    
+    if num ==2 :
+        return True
+
+    if num % 2 == 0:
+    	return False
+    
+    for i in range(3, int(num**0.5) + 1,2):
+        if num % i == 0:
+            return False
+    return True
+
+
+for number in range(limit):
+	if isPrime(number):
+		sumPrimes += number
+
+print(sumPrimes)
+
