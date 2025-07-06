4fadb424c26863585d6d3422f124f28b457efe82
Thomas K <thomas.karlsen@gmail.com>
2025-07-06 10:35:14 +0200

    Corrected typo, added docstring and pep8-style

--- problem4.py
+++ problem4.py
@@ -2,36 +2,52 @@
 A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
 
 Find the largest palindrome made from the product of two 3-digit numbers.
+
+Attributes:
+    largest_palindrome (int): The largest palindrome found
+    palX (int): First factor of palindrome 
+    palY (int): Second factor of palindrome
 '''
 
-#solved
+# solved
+
 
 def isPalindrome(num):
-	original_num = num
-	reverse = 0
+    """Checks whether a number num is a palindrome
 
-	while num > 0:
-		digit = num % 10
-		reverse = reverse * 10 + digit
-		num //= 10
+    Args:
+        num (INT): parameter to be checked if is a palindrome
 
-	if reverse == original_num:
-		return True
-	else:
-		return False
+    Returns:
+        Boolean: TRUE if if num is palindrome. False if number is not palindrome
+    """
+    original_num = num
+    reverse = 0
+
+    while num > 0:
+        digit = num % 10
+        reverse = reverse * 10 + digit
+        num //= 10
+
+    if reverse == original_num:
+        return True
+    else:
+        return False
+
 
 palY = 0
 palX = 0
-larges_palindrome = 0
+largest_palindrome = 0
 
-for x in range(999,99,-1):
-	for y in range(999,99,-1):
-		product = x*y
-		if (isPalindrome(x*y) and product > larges_palindrome):
-			larges_palindrome = product
-			palY = y
-			palX = x
-			
-print("Largest palindrome is " + str(larges_palindrome) + " and consists of " + str(palX) + " and " + str(palY) )
+for x in range(999, 99, -1):
+    for y in range(999, 99, -1):
+        product = x*y
+        if (isPalindrome(x*y) and product > largest_palindrome):
+            largest_palindrome = product
+            palY = y
+            palX = x
 
-#solved
+print("Largest palindrome is " + str(largest_palindrome) +
+      " and consists of " + str(palX) + " and " + str(palY))
+
+# solved
--- problem5.py
+++ problem5.py
@@ -2,24 +2,39 @@
 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+Attributes:
+    found (bool): Description
+    i (int): Description
 """
 
 # solved
 
+
 def testDivisibility(divident, divisors):
-	for i in range(1,divisors+1):
-		if divident % i != 0:
-			return False
-	return True
+    """Summary
+
+    Args:
+        divident (int): number to be tested whether is divisible by all numbers in divisors
+        divisors (int): upper bound for list of divisors 
+
+    Returns:
+        boolean: returns whether divident is divisible by all divisors
+    """
+    for i in range(1, divisors+1):
+        if divident % i != 0:
+            return False
+    return True
+
 
 i = 1
 found = False
 
 while found != True:
-	if testDivisibility(i,20):
-		print(i)
-		found = True
-		break
-	i += 1
+    if testDivisibility(i, 20):
+        print(i)
+        found = True
+        break
+    i += 1
 
 # solved
